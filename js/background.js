9a0be1fe164fb420313f0c1647fec2476f5e3f90
Cody Chan <int64ago@gmail.com>
2015-04-07 18:16:55 +0800

    Fix JSLint warnning

--- js/background.js
+++ js/background.js
@@ -1,48 +1,62 @@
-chrome.runtime.onInstalled.addListener(function() {
-	chrome.contextMenus.create({
-		'type':'normal',
-		'title':'Get Link!',
-		'contexts':["image"],
-		'id':'gl',
-	});
-});
+/*global chrome, Notification */
+/*jshint nonew: true */
 
-function showMsg(msgTitle, msgIcon, msgBody) {
-  new Notification(msgTitle, {
-    icon: msgIcon,
-    body: msgBody
-  });
-}
-
-function getlink(url){
-	var picUrl = 'http://getlink.sinaapp.com/ext?url='+url;
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", picUrl, true);
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState == 4) {
-			var retUrl = xhr.responseText;
-			if(retUrl != 'none' && retUrl.match(".com") != null){
-				copyTextToClipboard(retUrl);
-				showMsg("成功生成外链:-)", "images/icon48.png", "链接已经复制到剪贴板");
-			}else{
-				showMsg("生成外链失败！", "images/icon48_red.png", "请下载后在http://getlink.sinaapp.com/上传");
-			}
-		}
-	}
-	xhr.send();
-}
-
-chrome.contextMenus.onClicked.addListener(function(info, tab) {
-    if (info.menuItemId == 'gl')
-        getlink(info.srcUrl);
-});
 
 function copyTextToClipboard(text) {
-	var copyFrom = document.createElement("textarea");
+    "use strict";
+    var copyFrom, body;
+	copyFrom = document.createElement("textarea");
 	copyFrom.textContent = text;
-	var body = document.getElementsByTagName('body')[0];
+	body = document.getElementsByTagName('body')[0];
 	body.appendChild(copyFrom);
 	copyFrom.select();
 	document.execCommand('copy');
 	body.removeChild(copyFrom);
 }
+
+chrome.runtime.onInstalled.addListener(function () {
+    "use strict";
+	chrome.contextMenus.create({
+		'type': 'normal',
+		'title': 'Get Link!',
+		'contexts': [ "image" ],
+		'id': 'gl'
+	});
+});
+
+function showMsg(msgTitle, msgIcon, msgBody) {
+    "use strict";
+    var notification = new Notification(msgTitle, {
+        icon: msgIcon,
+        body: msgBody
+    });
+}
+
+function getlink(url) {
+    "use strict";
+    var picUrl, xhr;
+	picUrl = 'http://getlink.sinaapp.com/ext?url=' + url;
+	xhr = new XMLHttpRequest();
+	xhr.open("GET", picUrl, true);
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState === 4) {
+			var retUrl = xhr.responseText;
+			if (retUrl !== 'none' && retUrl.match(".com") !== null) {
+				copyTextToClipboard(retUrl);
+				showMsg("成功生成外链:-)", "images/icon48.png", "链接已经复制到剪贴板");
+			} else {
+				showMsg("生成外链失败！", "images/icon48_red.png", "请下载后在http://getlink.sinaapp.com/上传");
+			}
+		}
+	};
+	xhr.send();
+}
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+    "use strict";
+    if (info.menuItemId === 'gl') {
+        getlink(info.srcUrl);
+    }
+});
+
+
