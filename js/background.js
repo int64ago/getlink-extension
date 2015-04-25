1f0d4dfb56eb88062bc0b7c31f7f1dc44f5f18c7
Cody Chan <int64ago@gmail.com>
2015-04-25 21:25:59 +0800

    v2.0.0 for new site

--- js/background.js
+++ js/background.js
@@ -1,62 +1,70 @@
 /*global chrome, Notification */
 /*jshint nonew: true */
 
-
-function copyTextToClipboard(text) {
+(function () {
     "use strict";
-    var copyFrom, body;
-	copyFrom = document.createElement("textarea");
-	copyFrom.textContent = text;
-	body = document.getElementsByTagName('body')[0];
-	body.appendChild(copyFrom);
-	copyFrom.select();
-	document.execCommand('copy');
-	body.removeChild(copyFrom);
-}
-
-chrome.runtime.onInstalled.addListener(function () {
-    "use strict";
-	chrome.contextMenus.create({
-		'type': 'normal',
-		'title': 'Get Link!',
-		'contexts': [ "image" ],
-		'id': 'gl'
-	});
-});
-
-function showMsg(msgTitle, msgIcon, msgBody) {
-    "use strict";
-    var notification = new Notification(msgTitle, {
-        icon: msgIcon,
-        body: msgBody
-    });
-}
-
-function getlink(url) {
-    "use strict";
-    var picUrl, xhr;
-	picUrl = 'http://getlink.sinaapp.com/ext?url=' + url;
-	xhr = new XMLHttpRequest();
-	xhr.open("GET", picUrl, true);
-	xhr.onreadystatechange = function () {
-		if (xhr.readyState === 4) {
-			var retUrl = xhr.responseText;
-			if (retUrl !== 'none' && retUrl.match(".com") !== null) {
-				copyTextToClipboard(retUrl);
-				showMsg("成功生成外链:-)", "images/icon48.png", "链接已经复制到剪贴板");
-			} else {
-				showMsg("生成外链失败！", "images/icon48_red.png", "请下载后在http://getlink.sinaapp.com/上传");
-			}
-		}
-	};
-	xhr.send();
-}
-
-chrome.contextMenus.onClicked.addListener(function (info, tab) {
-    "use strict";
-    if (info.menuItemId === 'gl') {
-        getlink(info.srcUrl);
+    function copyTextToClipboard(text) {
+        var copyFrom, body;
+        copyFrom = document.createElement("textarea");
+        copyFrom.textContent = text;
+        body = document.getElementsByTagName('body')[0];
+        body.appendChild(copyFrom);
+        copyFrom.select();
+        document.execCommand('copy');
+        body.removeChild(copyFrom);
     }
-});
 
+    chrome.runtime.onInstalled.addListener(function () {
+        chrome.contextMenus.create({
+            'type': 'normal',
+            'title': 'Get Link!',
+            'contexts': [ "image" ],
+            'id': 'gl'
+        });
+    });
+
+    function showMsg(isOK) {
+        var notification = new Notification(isOK ? 'Getlink successfully :-)' : 'Failed to getlink!', {
+            icon: isOK ? 'images/icon48.png' : 'images/icon48_red.png',
+            body: isOK ? 'The link is now in your clipboard' : 'Please upload in getlink.int64ago.org'
+        });
+    }
+
+    function getlink(callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'http://qiniu.coding.io/uptoken', true);
+        xhr.onreadystatechange = function (e) {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    callback(xhr.responseText);
+                } else {
+                    showMsg(false);
+                }
+            }
+        };
+        xhr.send();
+    }
+
+    chrome.contextMenus.onClicked.addListener(function (info, tab) {
+        if (info.menuItemId === 'gl') {
+            getlink(function (upToken) {
+                chrome.tabs.query({
+                    active: true,
+                    currentWindow: true
+                }, function (tabs) {
+                    chrome.tabs.sendMessage(tabs[0].id, {
+                        picInfo: {srcUrl: info.srcUrl, pageUrl: info.pageUrl, token: upToken}
+                    }, function (response) {
+                        if (response.result && response.result !== 'error') {
+                            copyTextToClipboard('http://int64ago.qiniudn.com/' + response.result);
+                            showMsg(true);
+                        } else {
+                            showMsg(false);
+                        }
+                    });
+                });
+            });
+        }
+    });
+}());
 
